35e7ece6f6a767de5a0e56441ded74040714d279
Santiago Moncada <santiago.m.a@hotmail.com>
2023-02-05 00:12:00 +0100

    day6

---
+++ day6/part1.js
@@ -0,0 +1,18 @@
+const { readFileSync } = require("fs");
+
+const INPUT = readFileSync("./input.txt", "utf8");
+
+const checkRepeat = (str) => {
+  return str.length !== new Set(str).size;
+};
+
+const packet = 3;
+
+for (let i = packet; i < INPUT.length; i++) {
+  console.log(INPUT.slice(i - packet, i + 1));
+  console.log(checkRepeat(INPUT.slice(i - packet, i + 1)));
+  if (!checkRepeat(INPUT.slice(i - packet, i + 1))) {
+    console.log(i + 1);
+    break;
+  }
+}
