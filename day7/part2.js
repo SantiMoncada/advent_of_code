945b7471f91a5b438a6d2c64f526ae6d1006d926
Santiago Moncada <santiago.m.a@hotmail.com>
2023-02-11 18:08:36 +0100

    day7

---
+++ day7/part2.js
@@ -0,0 +1,108 @@
+const { readFileSync } = require("fs");
+
+const INPUT = readFileSync("./input.txt", "utf8");
+
+const parsedInput = INPUT.split("\n");
+
+class treeNode {
+  constructor(name, parent) {
+    this.name = name;
+    this.totalSize = 0;
+    this.files = [];
+    this.parent = parent;
+    this.childs = [];
+  }
+
+  addChild(node) {
+    this.childs.push(node);
+  }
+
+  addFile(file) {
+    this.files.push(file);
+  }
+
+  findChild(nodeName) {
+    return this.childs.find((node) => {
+      return node.name === nodeName;
+    });
+  }
+}
+
+const root = new treeNode("root", null);
+let currentNode = root;
+
+for (let i = 0; i < parsedInput.length; i++) {
+  const line = parsedInput[i];
+
+  command = line.split(" ");
+
+  if (command[0] === "$") {
+    switch (command[1]) {
+      case "ls":
+        break;
+      case "cd":
+        switch (command[2]) {
+          case "/":
+            currentNode = root;
+            break;
+          case "..":
+            currentNode = currentNode.parent;
+
+            break;
+          default:
+            const name = command[2];
+
+            const output = currentNode.findChild(name);
+            currentNode = currentNode.findChild(name);
+            break;
+        }
+    }
+  } else if (command[0] === "dir") {
+    //dir
+    const dir = new treeNode(command[1], currentNode);
+    currentNode.addChild(dir);
+  } else {
+    //file
+    const file = command;
+    currentNode.addFile(file);
+  }
+}
+
+const calculateTotalSize = (node) => {
+  let total = 0;
+  node.files.forEach((file) => {
+    total += Number(file[0]);
+  });
+
+  node.childs.forEach((child) => {
+    total += Number(calculateTotalSize(child));
+  });
+
+  node.totalSize = total;
+
+  return node.totalSize;
+};
+
+calculateTotalSize(root);
+
+const totalDisk = 70000000;
+const spaceNeeded = 30000000;
+const spaceUsed = root.totalSize;
+
+const freeSpace = totalDisk - spaceUsed;
+
+const spaceToFree = spaceNeeded - freeSpace;
+
+let total = Infinity;
+const dfs = (node) => {
+  console.log("name->", node.name, " size->", node.totalSize);
+  if (node.totalSize >= spaceToFree && node.totalSize < total) {
+    total = node.totalSize;
+  }
+  node.childs.forEach((child) => {
+    dfs(child);
+  });
+};
+
+dfs(root);
+console.log({ total });
