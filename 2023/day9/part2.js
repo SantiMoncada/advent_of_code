5c1ea1590f6ede6e777ea63dcac119e1ab7c6200
Santiago Moncada <santiago.m.a@hotmail.com>
2024-05-01 13:32:17 +0200

    day9part1

---
+++ 2023/day9/part2.js
@@ -0,0 +1,58 @@
+//@ts-check
+const { readFileSync } = require("fs");
+
+const INPUT = readFileSync("./input.txt", "utf8");
+
+/**
+ * @param {string} input
+ * @returns {number[][]}
+ */
+function parseInput(input) {
+  const lines = input.trim().split("\n");
+
+  const ouput = [];
+  for (const line of lines) {
+    ouput.push(line.split(" ").map((e) => parseInt(e)));
+  }
+
+  return ouput;
+}
+
+const reports = parseInput(INPUT);
+
+/**
+ * @param {number[]} report
+ * @returns {number}
+ */
+function predictNextValue(report) {
+  //all 0's base case
+  for (let i = 0; i < report.length; i++) {
+    if (report[i] !== 0) {
+      break;
+    }
+
+    if (i === report.length - 1) {
+      return 0;
+    }
+  }
+
+  /**
+   * @type {number[]}
+   */
+  const diference = [];
+  for (let i = 0; i < report.length - 1; i++) {
+    diference[i] = report[i + 1] - report[i];
+  }
+
+  return predictNextValue(diference) + report[report.length - 1];
+}
+
+const out = predictNextValue(reports[0]);
+
+let count = 0;
+for (const report of reports) {
+  const prediction = predictNextValue(report);
+  console.log(prediction);
+  count += prediction;
+}
+console.log({ count });
