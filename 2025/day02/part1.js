9ef6887585bdb9f2e24ff6d2de844116c4852d9b
santiMoncada <santiago.m.a@hotmail.com>
2026-02-10 03:06:55 +0100

    day2

--- 2025/day02/part1.js
+++ 2025/day02/part1.js
@@ -6,7 +6,6 @@ const INPUT = readFileSync("./input.txt", "utf8");
 
 const blocks = INPUT.split("\n")[0].split(",");
 
-console.log(blocks)
 
 /**
 *@param {string} str
@@ -24,22 +23,53 @@ function checkPair(str) {
   return true
 }
 
-let sum = 0
+/**
+*@param {string} str
+*/
+function checkPatterns(str) {
+  const len = str.length
+
+  let foundPatterns = 0
+  for (let i = 1; i <= len / 2; i++) {
+    if (len % i !== 0) {
+      continue
+    }
+    const slidingWindowSize = i
+    const matchWindow = str.slice(0, slidingWindowSize)
+
+    let matches = true
+    for (let j = len / slidingWindowSize - 1; j >= 0; j--) {
+      const extractedString = str.slice(j * slidingWindowSize, j * slidingWindowSize + slidingWindowSize)
+      if (matchWindow !== extractedString) {
+        matches = false
+        break
+      }
+    }
+    if (matches) {
+      foundPatterns++
+    }
+  }
+  return foundPatterns
+}
+let part1 = 0
+let part2 = 0
+console.clear()
+
+console.log(blocks)
 for (const block of blocks) {
 
   const [start, end] = block.split("-")
 
-  console.log({ start, end })
-
+  console.log(block)
   for (let i = parseInt(start); i <= parseInt(end); i++) {
+    if (checkPatterns(i.toString())) {
+      part2 += i
+    }
     if (checkPair(i.toString())) {
-      sum += i
-      console.log(i)
+      part1 += i
     }
   }
 }
 
-
-console.log("sum " + sum)
-
-5666095551 //to low
+console.log("part1 = " + part1)
+console.log("part2 = " + part2)
